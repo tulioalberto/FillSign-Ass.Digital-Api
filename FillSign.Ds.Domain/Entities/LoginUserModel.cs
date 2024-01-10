c61b621b4c6f87dc9bbe005e10ddb189b9d6419e
Tulio <tulio.koca@gmail.com>
2024-01-10 14:55:09 -0300

    Criando Controller de autenticação

---
+++ FillSign.Ds.Domain/Entities/LoginUserModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FillSign.Ds.Domain.Entities
+{
+    public class LoginUserModel
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Password { get; set; }
+    }
+}
